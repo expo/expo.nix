9096db0a2aa0537f3fd51c20db9e9d2deb011dc2
Nick Novitski <github@nicknovitski.com>
2024-12-09 15:37:26 -0800

    fix(yarn): set libc supported architectures

--- shell-modules/yarn-nixify.nix
+++ shell-modules/yarn-nixify.nix
@@ -6,17 +6,38 @@
 }:
 {
   options.expo.yarn-nixify.enable = lib.mkEnableOption "yarn-nixify helper";
-  config = lib.mkIf config.expo.yarn-nixify.enable {
-    packages = [
-      (pkgs.writeShellApplication {
-        name = "yarn-nixify";
-        text = ''
-          yarn plugin import https://raw.githubusercontent.com/stephank/yarn-plugin-nixify/main/dist/yarn-plugin-nixify.js
-          yarn config set supportedArchitectures --json '{"os":["darwin","linux"],"cpu":["arm64","x64"],"libc":["current"]}'
-          yarn install
-        '';
-        meta.description = "Configure yarn to explicitly support all platforms we run and build on.";
-      })
-    ];
+  options.expo.yarn-nixify.supportedArchitectures = lib.mkOption {
+    default = {
+      os = [
+        "darwin"
+        "linux"
+      ];
+      cpu = [
+        "arm64"
+        "x64"
+      ];
+      libc = [
+        "glibc"
+        "musl"
+      ];
+    };
+    type = lib.types.anything;
   };
+  config =
+    let
+      cfg = config.expo.yarn-nixify;
+    in
+    lib.mkIf cfg.enable {
+      packages = [
+        (pkgs.writeShellApplication {
+          name = "yarn-nixify";
+          text = ''
+            yarn plugin import https://raw.githubusercontent.com/stephank/yarn-plugin-nixify/main/dist/yarn-plugin-nixify.js
+            yarn config set supportedArchitectures --json '${builtins.toJSON cfg.supportedArchitectures}'
+            yarn install
+          '';
+          meta.description = "Configure yarn to explicitly support all platforms we run and build on.";
+        })
+      ];
+    };
 }
